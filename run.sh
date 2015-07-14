c39fd8fa74859d1475df49fc66a4411d19bc45a1
Julian Löhr <julian.loehr@outlook.com>
2015-07-14 11:01:01 +0200

    Added dated log file as output

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-mpiexec -n 4 --output-filename ./out/log ./bin/bitonic-mpi -t 65536 -n 1 -f ./data/65536tweets -o ./out/  "la"
+#!/bin/bash
+
+DATE=`date +%Y-%m-%d_%H-%M-%S`
+
+mpiexec -n 4 --output-filename ./out/log.$DATE ./bin/bitonic-mpi -t 65536 -n 1 -f ./data/65536tweets -o ./out/  "la"
 #/usr/bin/time -l mpiexec -n 2 ./bin/bitonic-mpi -t 2097152 -n 8 -f ./data/2097152tweets  "la"
 #mpiexec -n 2 ./bin/bitonic-mpi -t 2097152 -n 1 -f ./data/2097152tweets  "la"
 #./bin/bitonic-mpi -t 65536 -n 1 -f ./data/65536tweets  "la"
