91750e4e021033940fcdc00b2269be0eb7f41134
Julian Löhr <julian.loehr@outlook.com>
2015-07-16 14:32:15 +0200

    Adjusted run bash script

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 DATE=`date +%Y-%m-%d_%H-%M-%S`
 
-mpiexec -n 4 --output-filename ./out/log.$DATE ./bin/bitonic-mpi -t 65536 -n 1 -f ./data/65536tweets -o ./out/  "la"
+mpiexec -n 8 ./bin/bitonic-mpi -t 65536 -n 1 -f ./data/65536tweets -o ./out/  "la" > ./out/log
 #/usr/bin/time -l mpiexec -n 2 ./bin/bitonic-mpi -t 2097152 -n 8 -f ./data/2097152tweets  "la"
-#mpiexec -n 2 ./bin/bitonic-mpi -t 2097152 -n 1 -f ./data/2097152tweets  "la"
+#mpiexec -n 4 ./bin/bitonic-mpi -t 2097152 -n 1 -f ./data/2097152tweets  "la"
 #./bin/bitonic-mpi -t 65536 -n 1 -f ./data/65536tweets  "la"
 #./bin/bitonic-mpi -t 2097152 -n 8 -f ./data/2097152tweets  "la"
